36283f3ada25713a41e61843879d29a2625521b9
Olle E. Johansson <oej@edvina.net>
2021-04-07 11:01:39 +0200

    Allow for IPv6 in logs
    - update print statements to python 3 syntax

--- parse-iptlog.py
+++ parse-iptlog.py
@@ -1,3 +1,9 @@
+# Parse IPtables logs
+# Based on code from https://github.com/jakgibb/parse-iptables-log
+# released under the "Do What The F*ck You Want To Public License" 
+
+# Modified by Olle E. Johansson - oej@edvina.net
+# 
 import re
 import fileinput
 from collections import Counter
@@ -10,30 +16,45 @@ for line in fileinput.input():
     line = line.rstrip()
     data = dict(pair_re.findall(line))
     date = line.split()
-    portlist.append(data['DPT'])
+    print("DEBUG: Data {}".format(data))
+    if data.get('DPT', None) == None:
+        portlist.append(data['PROTO'])
+        plabel="PROTO"
+        pvalue=data['PROTO']
+    else:
+        print("DEBUG: Data DPT {}".format(data['DPT']))
+        portlist.append(data['DPT'])
+        plabel="Dport"
+        pvalue=data['DPT']
+    if data.get('SPT', None) == None:
+        splabel=""
+        spvalue=""
+    else:
+        splabel="SPT"
+        spvalue=data['SPT']
     iplist.append(data['SRC'])
-    print date[0]+" "+date[1]+" "+date[2]+" ",
-    print data['PROTO'], "\tDPT:", data['DPT'], "\tSPT:", data['SPT'], "\tSRC:", data['SRC'], "\tDST:", data['DST']
+    print(date[0]+" "+date[1]+" "+date[2]+" ",)
+    print(data['PROTO'], "\t",plabel, pvalue, "\t",splabel, spvalue, "\tSRC:", data['SRC'], "\tDST",data['DST'])
 
 try:
-    print "\n"
+    print("\n")
     count = 0
     linesToShow = 10
     while (count < linesToShow):
         portcommon = [ite for ite, it in Counter(portlist).most_common()]
-        print "Port: "+str(portcommon[count])+" had "+str(portlist.count(portcommon[count]))+" hits"
+        print ("Port: "+str(portcommon[count])+" had "+str(portlist.count(portcommon[count]))+" hits")
         count = count + 1
 
 except IndexError:
-    print ""
+    print("")
 
 try:
-    print "\n"
+    print("\n")
     count = 0
     while (count < linesToShow):
         ipcommon = [ite for ite, it in Counter(iplist).most_common()]
-        print "IP: "+str(ipcommon[count])+" had "+str(iplist.count(ipcommon[count]))+" hits"
+        print("IP: "+str(ipcommon[count])+" had "+str(iplist.count(ipcommon[count]))+" hits")
         count = count + 1
 except IndexError:
-    print ""
-print "\n"
+    print ("")
+print("\n")
